f047a3df6a861156b9f6e4637c31a6a6bb2acae3
xXMarvin2Xx <marvinheise2@gmail.com>
2018-06-06 15:49:31 +0200

    Arduino Briefkasten öffnen per Knopfdruck
    Hinzufügen der Arduino-Datei zum öffnen des Briefkastens per Knopfdruck

---
+++ _öffnen_per_knopfdruck.ino
@@ -0,0 +1,37 @@
+
+
+/*
+Into Robotics
+*/
+ 
+#include <Servo.h> 
+ 
+int servoPin = 9;
+ 
+Servo servo;  
+int 
+int inPin = 7; // Pushbutton Pin.
+int val = 0; // Variable zum Lesen des Pin-Status 
+int servoAngle = 0;   // servo position in degrees
+ 
+void setup()
+{
+  Serial.begin(9600);  
+  servo.attach(servoPin);
+  pinMode(inPin, INPUT);    // declare pushbutton as input
+}
+ 
+ 
+void loop()
+{
+
+ val = digitalRead(inPin); //Auslesen ob Button gedrückt wurde.
+  if (val == LOW) //Wenn Button gedrückt wurde. 
+  {   
+   servo.write(45);      // öffnet den Briefkasten für 10 Sekunden
+   delay(10000);          // Wait 10 seconds
+   servo.write(90);      // Schließt den Briefkasten
+   delay(10000);          // Wait 1 second
+   
+  }
+}

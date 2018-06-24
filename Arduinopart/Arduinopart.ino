142101dbfae429a7edb61a88c34061ffd9505317
rtoosi <r.toosi@yahoo.com>
2018-06-24 12:40:46 +0200

    Mergebugfix
    Es wurden sämtliche überschüssige merge dateien gelöscht.

--- Arduinopart/Arduinopart.ino
+++ Arduinopart/Arduinopart.ino
@@ -8,32 +8,6 @@
 #include <Servo.h> /*Libraries*/
 #include "DumbServer.h" 
 #include <SoftwareSerial.h>
-
-<<<<<<< HEAD
-int servoPin1 = 9; //Pin für den Servo-Motor
-
-Servo servo1; //Ermöglicht Nutzung der Servo-Motor Bibliothek
-
-SoftwareSerial esp_serial(3, 2);
-EspServer esp_server;
-
-int Zaehler = 0; //Variable zum zählen der Anzahl der durchgeführten Öffnungen
-int inPin1 = 11; //Button Außerhalb des Briefkasten, öffnet Postschlitz
-int val1 = 0; //Variable zum Lesen des Buttonstatus des inneren Button
-int val2 = 0; //Variable zum lesen des Buttonstatus des äußeren Button
-int servoAngle1 = 0; //Winkel des Servo-Motors
-int inPin2 = 13; //Button innerhalb des Briefkasten, resetet gezählte Einwürfe
-int led = 12; //Led zum anzeigen ob Post im Briefkasten ist
-
-void setup() {
-  esp_serial.begin(9600);
-  Serial.begin(9600);
-  servo1.attach(servoPin1); //Verknüpft den Pin des Servo-Motors mit der Servo-Motor Bibliothek
-  servo1.write(+90); //Setzt den Winkel auf Ausgangsposition
-  pinMode(inPin1, INPUT_PULLUP); //Der Äußere Button wird als Input definiert
-  pinMode(inPin2, INPUT_PULLUP); //Der innere Button wird als Input definiert
-  pinMode(led, OUTPUT); //Die Led wird als Output definiert
-=======
 /**The Wifi shield is connected to 
  * the Arduino-Pins 2 and 3
  */
@@ -64,7 +38,6 @@ void setup() //!<setup the wifi-shied + server. additionally declare Pin as Outp
   pinMode(led, OUTPUT);          /** declare LED as Output */
 
   Serial.println("Starting server..."); 
->>>>>>> 9c8007933246709a87d16cdbf0c47b51f791ca3d
   esp_server.begin(&esp_serial, "AndroidAP", "fdqz0801", 30303);
   Serial.println("...server is running");
   /**Get and print the IP-Address to which the Python should connect to */ 
@@ -74,32 +47,7 @@ void setup() //!<setup the wifi-shied + server. additionally declare Pin as Outp
   Serial.print("My ip: ");
   Serial.println(ip);
 }
-<<<<<<< HEAD
-void loop() {
-  millis(); //Startet einen Dauerhaft steigenden Zähler
-  val1 = digitalRead(inPin1); //Auslesen ob Button zum öffnen des Briefschlitzes gedrückt wurde
-  val2 = digitalRead(inPin2); // Auslesen ob Button zum restten des öffnungs-Zählers
 
-  if (val1 == LOW) { //Wenn der äußere Button gedrückt wurde
-    delay(1000); //Kurze Pause vor öffnen des Briefschlitzes
-    servo1.write(0); //Der Servo-Motor wir auf die Audgangsposition Gesetzt
-    delay(10000); //Pause vor schließen des Postschlitzes
-    servo1.write(+90); //Der Servo-Motor schließt den Briefschlitz
-    Zaehler++ ; //Anzahl der Einwürfe erhöht sich um eins
-    esp_server.write("y\n");
-  }
-
-  if (val2 == LOW) { //Wenn der innere Button gedrückt wurde.
-    Zaehler = 0; //Setzt die Anzahl der Einwürfe auf null zurück
-    esp_server.write("n\n");
-  }
-
-  if (Zaehler >= 1) {
-    digitalWrite(led, HIGH); //Led leuchtet wenn Briefe eingeworfen wurden
-  }
-  else {
-    digitalWrite(led, LOW); //Led hört auf zu leuchten wenn die Anzahl der Einwürfe resetet wurde
-=======
 void loop() //<! Start Loop
 {
 
@@ -128,7 +76,7 @@ void loop() //<! Start Loop
   else 
   {
     esp_server.write("n\n");  /** say n to the client */
->>>>>>> 9c8007933246709a87d16cdbf0c47b51f791ca3d
+
   }
 }
 
